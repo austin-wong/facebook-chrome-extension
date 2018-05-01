e315540d1e26327c7597a8ddab7e304531d1f82e
austin-wong <austinwong001@gmail.com>
2018-04-30 17:06:51 -0700

    Revert "alert to confirm"
    This reverts commit 2eee7930ed0a3b3b938f7c70157e1d39c22fd5b0.

--- content.js
+++ content.js
@@ -15,6 +15,6 @@ function postWarning(length) {
 
 document.querySelectorAll('a[data-testid="fb-ufi-likelink"]').forEach(function(element) {
 		element.addEventListener('click', function() {
-			confirm("You clicked like");
+			alert("You clicked like");
 	})
 });
