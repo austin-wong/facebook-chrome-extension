68d0cb6290a07280c7a2044d016b3f52e594bee6
austin-wong <austinwong001@gmail.com>
2018-08-06 17:05:37 -0700

    added function to repeatedly call function

--- content.js
+++ content.js
@@ -1,8 +1,9 @@
 // content.js
 
 
-confirm("<insert question here>?");
+//alert("<insert question here>?13");
 // the confirmation script
+/*
 function postWarning(length) {
     var txt;
     if (confirm("You are about to like a post from " + length + " ")) {
@@ -12,9 +13,28 @@ function postWarning(length) {
     }
     document.getElementById("demo").innerHTML = txt;
 }
+*/
 
-document.querySelectorAll('a[data-testid="fb-ufi-likelink"]').forEach(function(element) {
-		element.addEventListener('click', function() {
-			alert("You clicked like");
-	})
-});
+
+window.setInterval(function() {
+    document.querySelectorAll('[data-testid="fb-ufi-likelink"]').forEach(function(element) 
+    {
+        element.addEventListener("click", function(event) {
+            var r = confirm("You clicked like");
+            if (r == true) {
+              alert("you clicked OK");     
+            } else {
+              alert("you clicked Cancel");
+              event.stopPropagation();
+            }           
+         }, false);
+     });
+}, 5000);
+
+
+
+
+
+
+
+    
